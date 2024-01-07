5deb3f0a5fa5a0d017ea624bf5f487e4f7858149
Stewart Worrall <stewart.worrall@sydney.edu.au>
2024-01-07 16:37:53 +1100

    basic assembly project for the introduction lab

---
+++ intro-lab/Src/assembly.s
@@ -0,0 +1,21 @@
+.syntax unified
+.thumb
+
+.global main
+
+.data
+@ define variables
+
+.text
+@ define code
+
+@ this is the entry function called from the c file
+main:
+
+	LDR R0, =0x1234
+	LDR R1, =0x0001
+
+forever_loop :
+
+	ADD R0 , R1
+	B forever_loop
