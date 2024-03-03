5a7b37d4f40de01df4ed43a9807f1bce31435996
Stewart Worrall <stewart.worrall@sydney.edu.au>
2024-03-03 17:38:41 +1100

    included some more memory definitions for L01 example

--- L01-basic-example/Src/assembly.s
+++ L01-basic-example/Src/assembly.s
@@ -6,11 +6,17 @@
 .data
 @ define variables
 
+byte_array_1: .byte 0,1,2,3,4,5,6,7
+word_array_1: .word 0x01234567
+
 .text
 @ define code
 
 @ this is the entry function called from the startup file
 main:
+
+	LDR R0, =byte_array_1
+	LDR R0, =word_array_1
 
 	LDR R0, =0x1234
 	LDR R1, =0x0001
